a855f44cc2434dcb7b3dc9c29a0e66b7b5fc47ae
Chylli <chylli.email@gmail.com>
2013-01-22 21:26:37 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubypgadmin::Application.config.secret_token = '0e203655566ba81f0256028f3d41191d52d0d47177b3091b1ad682f7fbb7fc7a7f8110be322e73d5142b8afd526d7e30526794954568c42793b4476918c35cb8'
